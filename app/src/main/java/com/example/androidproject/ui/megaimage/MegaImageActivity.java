c88874c4bd5ae934cdd8615554da56346262b93c
Angelescu Horia <asteam60@gmail.com>
2022-05-16 18:18:19 +0300

    addded shops

---
+++ app/src/main/java/com/example/androidproject/ui/megaimage/MegaImageActivity.java
@@ -0,0 +1,16 @@
+package com.example.androidproject.ui.megaimage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.androidproject.R;
+
+public class MegaImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mega_image);
+    }
+}
--- app/src/main/java/com/example/androidproject/ui/shops/ShopsActivity.java
+++ app/src/main/java/com/example/androidproject/ui/shops/ShopsActivity.java
@@ -1,24 +1,31 @@
 package com.example.androidproject.ui.shops;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.androidproject.R;
 import com.example.androidproject.databinding.ActivityShopsBinding;
+import com.example.androidproject.ui.login.LoginActivity;
+import com.example.androidproject.ui.megaimage.MegaImageActivity;
+import com.example.androidproject.ui.register.RegisterActivity;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class ShopsActivity extends AppCompatActivity {
-
     private ActivityShopsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shops);
 
         binding = ActivityShopsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -27,14 +34,10 @@ public class ShopsActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
         toolBarLayout.setTitle(getTitle());
+    }
 
-        FloatingActionButton fab = binding.fab;
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    public void getToMegaImage(View v) {
+        Intent intent = new Intent(getApplicationContext(), MegaImageActivity.class);
+        startActivity(intent);
     }
 }
